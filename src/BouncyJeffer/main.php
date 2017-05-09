abbbdff34f1e833dc976f275b1a05af2ff15450a
BouncyJeffer <BouncyJeffer@gmail.com>
2017-05-08 22:12:35 -0700

    Update main.php

--- src/BouncyJeffer/main.php
+++ src/BouncyJeffer/main.php
@@ -1,3 +1,20 @@
 <?php
 namespace BouncyJeffer;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+class main extends PluginBase {
+  public function onLoad(){
+    $this->getLogger()->info(TextFormat::YELLOW."Currently loading...");
+  }
+  public function onEnable(){
+    $this->getLogger()->info(TextFormat::GREEN."has been started!");
+  }
+  public function onDisable(){
+    $this->getLogger()->info(TextFormat::RED."disabled.");
+  }
+}
 ?>
