a1d3aadb0def97cbb36e71a5524663bdd0ddde82
BouncyJeffer <BouncyJeffer@gmail.com>
2017-05-09 09:46:13 -0700

    Added command "/code kickall" and plugin prefix.
    Kick all players off the server using command "/code kickall", this will kick all players besides you off the server.

--- src/BouncyJeffer/main.php
+++ src/BouncyJeffer/main.php
@@ -8,6 +8,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 class main extends PluginBase {
+  public $prefix = TextFormat::YELLOW."[".TextFormat::RED."CodeRed".TextFormat::YELLOW."] ".TextFornat::WHITE.";
   public function onLoad(){
     $this->getLogger()->info(TextFormat::YELLOW."Currently loading...");
   }
@@ -16,6 +17,29 @@ class main extends PluginBase {
   }
   public function onDisable(){
     $this->getLogger()->info(TextFormat::RED."disabled.");
+  }
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    switch(strtolower($command->getName()){
+      case "code":
+      if(count($args) < 1){
+        $sender->sendMessage($this->prefix."Invalid syntax. /code <operation>");
+      } else {
+        switch(strtolower($args[0])){
+          case "kickall":
+          $sender->sendMessage($this->prefix."Kicking all players.");
+            foreach($this->getServer()->getOnlinePlayers() as $p){
+            if($p !== $sender){
+            $p->kick("An emergency has ocurred on the server. Kicking all players.");
+            }
+            }
+          break;
+          default:
+            $sender->sendMessage($this->prefix."Unknown operation, do ".TextFormat::GRAY."/code ?".TextFormat::WHITE." for help.");
+          break;
+        }
+      }
+      break;
+    }
   }
 }
 ?>
